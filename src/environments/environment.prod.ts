8dbecdc29b13c872fb2ce99be20518621c7ed8d5
KiranMungekar <35713742+KiranMungekar@users.noreply.github.com>
2018-02-21 09:00:57 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyBmkFiJYRJq7f9J_jIgFUTf2JH5KXZX3dg',
-    authDomain: 'kanjurbarg-54fa6.firebaseapp.com',
-    databaseURL: 'https://kanjurbarg-54fa6.firebaseio.com',
-    projectId: 'kanjurbarg-54fa6',
-    storageBucket: 'kanjurbarg-54fa6.appspot.com',
-    messagingSenderId: '1093333107274',
+    apiKey: 'AIzaSyC7LCTdgviIbiGUUXLHaHg4kYEeRV1PHNE',
+    authDomain: 'digi-polls.firebaseapp.com',
+    databaseURL: 'https://digi-polls.firebaseio.com',
+    projectId: 'digi-polls',
+    storageBucket: 'digi-polls.appspot.com',
+    messagingSenderId: '584334078188',
   }
 };
