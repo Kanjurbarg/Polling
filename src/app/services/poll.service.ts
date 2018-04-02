1561b6a0786e2e61ae08e313d02612642a1ce0de
Pratik <pratikmm@hotmail.com>
2018-04-03 03:00:20 +0530

    Contenders Added Successfully

--- src/app/services/poll.service.ts
+++ src/app/services/poll.service.ts
@@ -2,20 +2,50 @@ import { Injectable } from '@angular/core';
 import * as firebase from 'firebase/app';
 import { AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection, fromDocRef } from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
+import { Route } from '@angular/compiler/src/core';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class PollService {
 
 
-  constructor(private afs:AngularFirestore) { }
+  constructor(private afs:AngularFirestore,private router:Router) { }
 
-  getPoll(){
-    return this.afs.collection('polls').valueChanges();
+  getPoll(pid){
+    return this.afs.doc('polls/' + pid).valueChanges();
   }
 
-  createPolls()
+  createPolls(pollDetails)
   {
-    
+    const pid =this.afs.createId();
+    const data={
+      title:pollDetails.title,
+      des:pollDetails.des,
+      gid:pollDetails.gid,
+      status:"off",
+      pid:pid,
+      createdOn:firebase.firestore.FieldValue.serverTimestamp(),
+      duration:pollDetails.duration,
+    };
+    this.afs.doc('polls/'+pid).set(data)
+    .then(()=>{
+      console.log("Success");
+      this.router.navigateByUrl('poll/'+pid);
+    }).catch((err)=>{
+      console.log(err);
+    });
+  }
+
+
+  addContenders(contender){
+    console.log("Service pid"+ contender.pid)
+    const data={
+      cid:contender.cid,
+      votes:contender.votes
+    };
+    this.afs.doc('polls/'+ contender.pid +'/contenders/'+data.cid).set(data).then(()=>{
+      console.log("Contender add successfully..");
+    })
   }
   /*getContenders(){
     return this.afs.coll
